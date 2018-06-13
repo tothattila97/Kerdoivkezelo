3f9531b51fc729b77142dd626351464e4e067e49
szbence26 <szokebenc@gmail.com>
2018-06-13 11:31:35 +0200

    Előző- és következő oldal gombok létrehozása

--- KerdoivKezelo/ClientApp/src/app/kerdoiv-lista/kerdoiv-lista.component.ts
+++ KerdoivKezelo/ClientApp/src/app/kerdoiv-lista/kerdoiv-lista.component.ts
@@ -7,11 +7,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class KerdoivListaComponent {
   public kerdoivek: Kerdoiv[];
+  maxElemszamEgyOldalon: number = 20;
+  utolsoOldal: number = 5;
+  oldalszam = 1;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Kerdoiv[]>(baseUrl + 'api/Kerdoiv/GetKerdoivek').subscribe(result => {
       this.kerdoivek = result;
     }, error => console.error(error));
+  }
+
+  kovetkezoOldal(): void {
+    if (this.oldalszam !== this.utolsoOldal) {
+      this.oldalszam++;
+    }
+    //TODO
+  }
+
+  elozoOldal(): void {
+    if (this.oldalszam !== 1) {
+      this.oldalszam--;
+    }
+    //TODO
   }
 
 }
