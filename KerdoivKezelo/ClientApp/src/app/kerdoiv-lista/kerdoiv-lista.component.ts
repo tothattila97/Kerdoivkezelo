b949933537b8fd8a1724f7478ef80c9d206d0887
szbence26 <szokebenc@gmail.com>
2018-06-13 10:25:24 +0200

    Kerdőívek listázása működik, átnevezések, url magyarítás

--- KerdoivKezelo/ClientApp/src/app/kerdoiv-lista/kerdoiv-lista.component.ts
+++ KerdoivKezelo/ClientApp/src/app/kerdoiv-lista/kerdoiv-lista.component.ts
@@ -2,10 +2,10 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  selector: 'app-kerdoiv-lista',
+  templateUrl: './kerdoiv-lista.component.html'
 })
-export class FetchDataComponent implements OnInit{
+export class KerdoivListaComponent {
   public kerdoivek: Kerdoiv[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
@@ -14,9 +14,6 @@ export class FetchDataComponent implements OnInit{
     }, error => console.error(error));
   }
 
-  ngOnInit(): void {
-    
-  }
 }
 
 interface Kerdoiv {
