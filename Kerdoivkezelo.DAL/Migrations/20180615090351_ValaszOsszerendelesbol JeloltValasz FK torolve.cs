0125e9710af5a3164c8424a14b501db3eaf9d279
AUT\pavliczandris <pavlicz.andris@gmail.com>
2018-06-15 12:00:00 +0200

    Migration-ök pótolva

---
+++ Kerdoivkezelo.DAL/Migrations/20180615090351_ValaszOsszerendelesbol JeloltValasz FK torolve.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kerdoivkezelo.DAL.Migrations
+{
+    public partial class ValaszOsszerendelesbolJeloltValaszFKtorolve : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ValaszOsszerendelesek_JeloltValaszok_JeloltValaszId",
+                table: "ValaszOsszerendelesek");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "JeloltValaszId",
+                table: "ValaszOsszerendelesek",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ValaszOsszerendelesek_JeloltValaszok_JeloltValaszId",
+                table: "ValaszOsszerendelesek",
+                column: "JeloltValaszId",
+                principalTable: "JeloltValaszok",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ValaszOsszerendelesek_JeloltValaszok_JeloltValaszId",
+                table: "ValaszOsszerendelesek");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "JeloltValaszId",
+                table: "ValaszOsszerendelesek",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ValaszOsszerendelesek_JeloltValaszok_JeloltValaszId",
+                table: "ValaszOsszerendelesek",
+                column: "JeloltValaszId",
+                principalTable: "JeloltValaszok",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
