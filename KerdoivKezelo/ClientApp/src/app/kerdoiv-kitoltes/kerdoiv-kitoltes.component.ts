ce0ecf001b2dbc4a6a3ff3a9942a18a915cbad42
AUT\pavliczandris <pavlicz.andris@gmail.com>
2018-06-18 08:38:16 +0200

    kerdoiv-kitoltes component fix (enum miatt)

--- KerdoivKezelo/ClientApp/src/app/kerdoiv-kitoltes/kerdoiv-kitoltes.component.ts
+++ KerdoivKezelo/ClientApp/src/app/kerdoiv-kitoltes/kerdoiv-kitoltes.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 export class KerdoivKitoltesComponent implements OnInit {
 
   kerdoiv: Kerdoiv;
+  kerdesTipus = KerdesTipus;
 
   constructor(private route: ActivatedRoute) { }
 
