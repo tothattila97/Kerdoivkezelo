7284d95bc033374d82ac34d2833d43ed7f8cddcf
Rácz Zalán <racz1666@gmail.com>
2018-06-18 12:17:46 +0200

    felhasználók listázása beégetett adatokból

--- KerdoivKezelo/ClientApp/src/app/app.module.ts
+++ KerdoivKezelo/ClientApp/src/app/app.module.ts
@@ -9,10 +9,11 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { KerdoivListaComponent } from './kerdoiv-lista/kerdoiv-lista.component';
 import { BejelentkezesComponent } from './bejelentkezes/bejelentkezes.component';
 import { RegisztracioComponent } from './regisztracio/regisztracio.component';
-import { EredmenyGrafComponent } from './eredmeny-graf/eredmeny-graf.component';
+import { StatisztikakComponent } from './statisztikak/statisztikak.component';
 import { KerdesComponent } from './kerdes/kerdes.component';
 import { KerdoivKitoltesComponent } from './kerdoiv-kitoltes/kerdoiv-kitoltes.component';
 import { KerdoivSzerkesztesComponent } from './kerdoiv-szerkesztes/kerdoiv-szerkesztes.component';
+import { FelhasznaloListaComponent } from './felhasznalo-lista/felhasznalo-lista.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +22,11 @@ import { KerdoivSzerkesztesComponent } from './kerdoiv-szerkesztes/kerdoiv-szerk
       KerdoivListaComponent,
       BejelentkezesComponent,
       RegisztracioComponent,
-      EredmenyGrafComponent,
+      StatisztikakComponent,
       KerdesComponent,
       KerdoivKitoltesComponent,
-      KerdoivSzerkesztesComponent
+      KerdoivSzerkesztesComponent,
+      FelhasznaloListaComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -34,10 +36,11 @@ import { KerdoivSzerkesztesComponent } from './kerdoiv-szerkesztes/kerdoiv-szerk
       { path: '', component: KerdoivListaComponent },
       { path: 'bejelentkezes', component: BejelentkezesComponent },
       { path: 'regisztracio', component: RegisztracioComponent },
-      { path: 'eredmeny-graf', component: EredmenyGrafComponent },
+      { path: 'statisztikak', component: StatisztikakComponent },
       { path: 'kerdes/:id', component: KerdesComponent },
       { path: 'kerdoiv-kitoltes/:id', component: KerdoivKitoltesComponent },
-      { path: 'kerdoiv-szerkesztes/:id', component: KerdoivSzerkesztesComponent }
+      { path: 'kerdoiv-szerkesztes/:id', component: KerdoivSzerkesztesComponent },
+      { path: 'felhasznalo-lista', component: FelhasznaloListaComponent }
     ])
   ],
   providers: [],
